cab12c92fac92395809f308fce7eda1ff7e3963b
mphan6 <mphan6@myseneca.ca>
2018-10-24 18:34:25 -0400

    Accidentally added a function/test that already existed
    Corrected the mistake and added test case for a different function
    firstDigitSum

--- src/digitSum.test.js
+++ src/digitSum.test.js
@@ -1,34 +1,19 @@
-import { digitSum } from './digitSum';
+import {
+  digitSum
+} from './digitSum';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(digitSum()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(digitSum()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(digitSum()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(digitSum()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { digitSum(); }).toThrow();
- */
 
 describe('digitSum', () => {
   it('digitSum does this thing...', () => {
-    // TODO Your own assertion here
+    expect(digitSum(123)).toEqual(6)
   });
-
-  it('digitSum does that other thing...', () => {
-    // TODO Your own assertion here
+  it('digitSum does this thing...', () => {
+    expect(digitSum(99999)).toEqual(9)
   });
-
-  it('digitSum does a very cool thing...', () => {
-    // TODO Your own assertion here
+  it('digitSum does this thing...', () => {
+    expect(digitSum(4)).toEqual(4)
+  });
+  it('digitSum throw error at negative number', () => {
+    expect(() => digitSum(-7)).toThrow()
   });
 });
---
+++ src/firstDigitSum.test.js
@@ -0,0 +1,19 @@
+import { firstDigitSum } from './firstDigitSum';
+
+describe('firstDigitSum valid args', () => {
+  it('firstDigitSum does this thing...', () => {
+    expect(firstDigitSum(100008)).toEqual(9);
+    expect(firstDigitSum(325162)).toEqual(19);
+    expect(firstDigitSum(99999)).toEqual(45);
+    expect(firstDigitSum(10636408)).toEqual(28);
+  });
+
+  it('firstDigitSum invalid args', () => {
+    expect(() => firstDigitSum()).toThrow();
+    expect(() => firstDigitSum(0)).toThrow();
+    expect(() => firstDigitSum(51)).toThrow();
+    expect(() => firstDigitSum('hola')).toThrow();
+    expect(() => firstDigitSum([])).toThrow();
+    expect(() => firstDigitSum({})).toThrow();
+  });
+});
