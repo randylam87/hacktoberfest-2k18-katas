59fbf67d6b5431abc4dc371c54cad1ea6748e3f7
mphan6 <mphan6@myseneca.ca>
2018-10-24 18:14:16 -0400

    Added nthSumNine() functionality
    Added the functionality for nthSumNine() and test cases for digitSum

--- src/digitSum.js
+++ src/digitSum.js
@@ -1,18 +1,3 @@
-export const digitSum = (value) => {
+export const digitSum = () => {
   // TO IMPLEMENT IN ANOTHER PR
-  value = '' + value;
-  if (value.indexOf('-') !== -1) {
-    throw ('Negative Numbers not supported')
-  }
-  let digitSum = 0;
-  let firstDigit = parseInt(value[0], 10)
-  for (let i = 0; i < value.length; i++) {
-    digitSum += parseInt(value[i], 10);
-  }
-  if ((firstDigit * value.length) === digitSum) {
-    //repeated numbers
-    return firstDigit;
-  }
-  return digitSum;
-
 };
