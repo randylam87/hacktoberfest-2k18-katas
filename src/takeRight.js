099901a252b5d27f9b4be2844be58b4b7a37b735
shingz96 <cheekahshing96@gmail.com>
2018-10-24 22:16:49 +0800

    calc rectangle volume & add takeRight

---
+++ src/takeRight.js
@@ -0,0 +1,8 @@
+/*
+* Returns an array/string with n elements removed from the end while still keep the order. Default take 1 if no provide any number to take.
+* EG 1: takeRight( [1,2,3,4,5,6], 5 ) -> [2,3,4,5,6]  
+* EG 2: takeRight( "HACK", 3 ) -> "ACK" 
+*/
+export const takeRight = (arr, n=1) => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
