05fffa4bdbdf24f999e0e8a3ec2e3a9c962f3376
Josh Wood <wood2jp2@gmail.com>
2018-10-26 13:41:58 -0400

    added isValidEmailAddress kata and test, and completed isPalindromeStringOrNumber

--- src/isPalindromeStringOrNumber.js
+++ src/isPalindromeStringOrNumber.js
@@ -1,3 +1,6 @@
-export const isPalindromeStringOrNumber = () => {
+export var isPalindromeStringOrNumber = (stringOrNum, nullArg = null) => {
   // TO IMPLEMENT IN ANOTHER PR
+  if (!stringOrNum || typeof stringOrNum === "object" || nullArg) throw new Error('Argument is not string or number.');
+  let reverseString = String(stringOrNum).split('').reverse().join('')
+  return reverseString === String(stringOrNum) ? true : false
 };
