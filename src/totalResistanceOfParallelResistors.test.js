f96e26f2266ecf1c538b55af35fb563c97c9a019
Thanh Nguyen <thanh1094@yahoo.com>
2018-10-26 01:10:06 -0400

    "Add totalResistanceOfParallelResistors implementation"

--- src/totalResistanceOfParallelResistors.test.js
+++ src/totalResistanceOfParallelResistors.test.js
@@ -4,7 +4,7 @@ describe('totalResistanceOfParallelResistors', () => {
   it('should calculate the total resistance of parallel connected resistors...', () => {
     expect(totalResistanceOfParallelResistors(100, 100)).toEqual(50);
     expect(totalResistanceOfParallelResistors(200, 470, 220)).toEqual(85.67);
-    expect(totalResistanceOfParallelResistors(1000, 1000, 470, 100, 100)).toEqual(43.05);
+    expect(totalResistanceOfParallelResistors(1000, 1000, 470, 100, 100)).toEqual(41.45);
     expect(totalResistanceOfParallelResistors(100, 200, 300, 400, 500, 600, 700, 800)).toEqual(36.79);
   });
 
