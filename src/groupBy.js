d9f9a34fa65fec957d70705dc7c82f0341a3db4c
Pawel Kochanek <pawel.kochanek@gmail.com>
2018-10-27 08:15:21 +0200

    844: working for one function

--- src/groupBy.js
+++ src/groupBy.js
@@ -1,3 +1,24 @@
-export const groupBy = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const groupBy = (numbers, ...functions) => {
+  if (numbers === undefined) {
+    return {};
+  }
+
+  if (functions.length === 0) {
+    return { [functions[0]]: numbers };
+  } else {
+    const obj = {};
+
+    for (const func of functions) {
+      for (const number of numbers) {
+        if (!Array.isArray(obj[func(number)])) {
+          obj[func(number)] = [number];
+        } else {
+          obj[func(number)].push(number);
+        }
+        console.log(obj);
+      }
+    }
+
+    return obj;
+  }
 };
